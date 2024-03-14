7ab6a05aa57ff178c784528d040d973137b1c25c
Jacqueline <jacquigavi921@gmail.com>
2024-03-14 00:58:09 -0500

    Creacion del proyecto

---
+++ src/main/java/com/banquito/core/gestioncreditos/GestioncreditosApplication.java
@@ -0,0 +1,13 @@
+package com.banquito.core.gestioncreditos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioncreditosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioncreditosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banquito/core/gestioncreditos/GestioncreditosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquito.core.gestioncreditos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioncreditosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
